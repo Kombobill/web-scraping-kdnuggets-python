7eafe884bac0347d5df53eb47a9605fb5858931e
Kombobill <billmadru25@gmail.com>
2024-10-11 01:12:35 +0300

    lookin for a strings stored in tags

--- main.py
+++ main.py
@@ -17,3 +17,8 @@ parsedData = BeautifulSoup(htmlData, "html.parser")
 soup = BeautifulSoup("<h1> Welcome to KDnuggets! </h1>", "html.parser")
 # print(type(soup))....to this step...we covert the html to beatifulsoup for us to understand in python
 # print(type(soup.h1)) ... after the above step we look for the tag h1...
+
+# print(soup.h1.string)
+# print(type(soup.h1.string))...this last two is for gettin a string.stored in a tag in string format
+#  
+
