03e1be9b280b01f0ff5a9377c0b684c2c2971eb3
Kombobill <billmadru25@gmail.com>
2024-10-11 00:29:42 +0300

    parse html

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from bs4 import BeautifulSoup
 import requests
 
 url = "https://www.kdnuggets.com/"
 res = requests.get(url)
 htmlData = res.content
-print(htmlData)
+# print(htmlData)
+
+# //to this point, we extract raw html content.Text , paragraphs,anchortags,divs..all ..next task is to parse data for text and tags separately//
+# //un comment print(htmlData)..to validate the above step..
+
+parsedData = BeautifulSoup(htmlData, "html.parser")
+print(parsedData.prettify())
